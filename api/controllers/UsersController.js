8105af0b02a8c236a0b8c5f426635c42dc636d93
Khanh Duong <khanh1111527@gmail.com>
2015-10-06 15:05:47 +0700

    change dirname folder

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -5,7 +5,7 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 //var apiHelper = require(__dirname + '/../services/ApiHelper');
-var apiHelper = require('/app/api/services/ApiHelper');
+//var apiHelper = require('ApiHelper');
 module.exports = {
 	// a CREATE action
   create: function(req, res, next) {
